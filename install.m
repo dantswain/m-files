834a2ebd6fdb5a5b2434d51e94d1f0e07e686592
Dan Swain <dswain@princeton.edu>
2012-04-04 10:28:47 -0400

    Add better commenting to install.m

--- install.m
+++ install.m
@@ -1,7 +1,9 @@
-% install
+% INSTALL   Install these files into your Matlab path.
 %
 % Adds the path to these m-files to your Matlab path so that you can use
-% them like 'normal' Matlab functions
+% them like 'normal' Matlab functions (unless it is already there). 
+
+% DTS 4/2/12 - initial commit
 
 mfiles_path = fileparts(mfilename('fullpath'));
 
